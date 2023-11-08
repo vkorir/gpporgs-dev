d4ed70fdeb43090b1f20aa6af8742b56224f49aa
Victor Korir <vkkorir@gmail.com>
2023-11-07 23:07:39 -0800

    Configured Angular with Firebase Auth/Firestore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { environment } from 'src/environments/environment';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyA9jc44KXwQEW7Brfobtdk_aiuEzlG-Vd8',
+    authDomain: 'gpporgs-dev.firebaseapp.com',
+    projectId: 'gpporgs-dev',
+    storageBucket: 'gpporgs-dev.appspot.com',
+    messagingSenderId: '468194096907',
+    appId: '1:468194096907:web:aa6a0fb2e74743e2e7940e',
+  },
+};
