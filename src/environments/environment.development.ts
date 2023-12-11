dadee6415d7a9117ea05cc1de990181d99de81b0
Victor Korir <vkkorir@gmail.com>
2023-12-11 09:46:38 +0300

    Blanking out firebase development environment config

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: true,
+    firebaseConfig: {
+        apiKey: '',
+        authDomain: 'gpporgs-dev.firebaseapp.com',
+        projectId: 'gpporgs-dev',
+        storageBucket: '',
+        messagingSenderId: '',
+        appId: '',
+    },
+};
